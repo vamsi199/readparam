fc29ea734500beeee73e4b9171524cb1a8da38ee
vamsi199 <vamsi.1991992@gmail.com>
2017-09-05 21:42:43 -0400

    1st task completed

--- main.go
+++ main.go
@@ -21,7 +21,7 @@ func readparameter(w http.ResponseWriter, req *http.Request) {
 	dog.ID = parameter["id"]
 	json.NewEncoder(w).Encode(dog.ID)
 }
-func Qweryread(w http.ResponseWriter, req *http.Request) {
+func Readmultiplequeryparameters(w http.ResponseWriter, req *http.Request) {
 	fmt.Println("Hello", req.URL.RawQuery)
 	var dog dogs
 	f := req.URL.Query()
@@ -31,14 +31,14 @@ func Qweryread(w http.ResponseWriter, req *http.Request) {
 	fmt.Fprintln(w, "color=", dog.Color)
 
 }
-func Qwerywrite(w http.ResponseWriter, r *http.Request) {
+func Storedatafrombody(w http.ResponseWriter, r *http.Request) {
 	var dog dogs
 	f := r.Body
 	json.NewDecoder(f).Decode(&dog)
 	fmt.Fprintln(w, "breed=", dog.Breed)
 	fmt.Fprintln(w, "color=", dog.Color)
 }
-func Headerread(w http.ResponseWriter, r *http.Request) {
+func Readfromheader(w http.ResponseWriter, r *http.Request) {
 	var dog dogs
 	byte := r.Header.Get("Breed")
 	byte1 := r.Header.Get("Color")
@@ -53,9 +53,9 @@ func main() {
 	//router := mux.NewRouter()
 	//router.HandleFunc("/dogs/{id}",readparameter).Methods("GET")
 	//log.Fatal(http.ListenAndServe(":8081",router))
-	http.HandleFunc("/dog", Qweryread)
-	http.HandleFunc("/dogwrite", Qwerywrite)
-	http.HandleFunc("/dogheader", Headerread)
-	http.ListenAndServe(":8086", nil)
+	http.HandleFunc("/dog", Readmultiplequeryparameters) //read query parameters with multiple values
+	http.HandleFunc("/dogwrite", Storedatafrombody) //read the data from body (json data) and store it in a structure variables
+	http.HandleFunc("/dogheader", Readfromheader) //read value in a header and print it.
+	http.ListenAndServe(":8080", nil)
 
 }
